bf75bfb5a07a71b87939274a89ceeff5f81f11f6
deveshsoni7 <devesh.soniwork@gmail.com>
2025-11-16 11:59:52 +0530

    Added CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://rapid-quest-hackathon.vercel.app/",   // your real frontend URL
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
